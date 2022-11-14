0766907897f8fae043301dd3633a4a18cc86a1da
WhiteBlackGoose <wbg@angouri.org>
2022-11-14 15:07:30 +0300

    minor

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿//
-// Copyright (c) 2019-2022 Angouri.
-// AngouriMath is licensed under MIT.
-// Details: https://github.com/asc-community/AngouriMath/blob/master/LICENSE.md.
-// Website: https://am.angouri.org.
-//
-using AngouriMath;
+﻿using AngouriMath;
 using AngouriMath.Extensions;
 
 var cliArgs = System.Environment.GetCommandLineArgs();
