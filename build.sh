88d0a4fa33ff04e20948db79da32d9c13286b223
WhiteBlackGoose <wbg@angouri.org>
2022-11-14 14:57:00 +0300

    upload

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [[ -z $1 ]]; then
+    printf "Provide rid\n"
+    exit
+fi
+
+rm -r bin
+rm -r obj
+
+# dotnet publish \
+../../../../../programs/dotnet-folder/dotnet publish \
+-r $1 \
+-c release \
+-o ./publish-output \
+-p:SelfContained=true \
+-p:PublishAot=true \
+-p:PublishTrimmed=true \
+-p:TrimMode=full \
+-p:IlcInvariantGlobalization=true \
+-p:IlcOptimizationPreference=Speed \
+-p:IlcDisableReflection=false \
+-p:StripSymbols=true \
+-p:Version=$(cat ./VERSION/VERSION)
+
+mv ./publish-output/CLI ./publish-output/amcli
